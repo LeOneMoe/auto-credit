ceba37290fd0aed67b1d95d7728f4fa77991d2f1
LeOneMoe <baNANAmanchic@gmail.com>
2022-04-28 15:35:58 +0300

    repo annotation lol

--- server/src/main/java/stud/carcredit/repository/ClientRepo.java
+++ server/src/main/java/stud/carcredit/repository/ClientRepo.java
@@ -3,10 +3,12 @@ package stud.carcredit.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import stud.carcredit.model.Client;
 
 import java.util.List;
 
+@Repository
 public interface ClientRepo extends JpaRepository<Client, Long> {
     @Query(
             "select c from Client c " +
